4dee8a2b4a886b2c865bf49b269fca49378379b3
Pawel <pawel.tomczyk85@gmail.com>
2021-06-15 22:29:53 +0200

    overrided parameters

--- src/main/java/com/app/controller/TaskController.java
+++ src/main/java/com/app/controller/TaskController.java
@@ -49,8 +49,14 @@ public class TaskController {
     public void updateTask(@RequestBody TaskDto taskDto){
         Task task = taskMapper.mapToTask(taskDto);
         service.saveTask(task);
-
     }
+
+//    @PutMapping(value="updateTask", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public TaskDto updateTask(@RequestBody TaskDto taskDto){
+//        Task task = taskMapper.mapToTask(taskDto);
+//        Task savedTask = service.saveTask(task);
+//        return taskMapper.mapToTaskDto(savedTask);
+//    }
 
     @DeleteMapping("/deleteTask")
     public void deleteTask(@RequestParam Long taskId) throws TaskNotFoundException{
