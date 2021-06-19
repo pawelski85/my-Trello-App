635562e96970bea20bed74ec15974923a82405d8
Pawel <pawel.tomczyk85@gmail.com>
2021-06-19 17:22:52 +0200

    Trello Client added

---
+++ src/main/java/com/app/domain/TrelloBoardDto.java
@@ -0,0 +1,15 @@
+package com.app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Setter
+@Data
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
